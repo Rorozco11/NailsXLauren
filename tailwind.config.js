51f71931f5b16e224ee0abc9d398c80369a243bc
Rorozco11 <96450960+Rorozco11@users.noreply.github.com>
2025-01-05 16:58:51 -0500

    starting

---
+++ tailwind.config.js
@@ -0,0 +1,91 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        abeezee: ['Abeezee', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+      
+        '.btn-bookNow': {
+          padding: '12px 24px',
+          fontSize: '1rem',
+          fontFamily: 'abeezee',
+          fontWeight: '0',
+          color: 'white',
+          border: '2px solid #A5B4FC',
+          borderRadius: '9999px', // Full rounding
+          backgroundColor: '#A5B4FC',
+          overflow: 'hidden',
+          cursor: 'pointer',
+        },
+        '.btn-bookNow:hover': {
+          color: ' #A5B4FC',
+          transform: 'scale(1.1)',
+          boxShadow: '0 0px 20px rgba(193, 163, 98, 0.4)',
+        },
+        '.btn-bookNow::before': {
+          content: '""',
+          position: 'absolute',
+          inset: '0',
+          width: '100%',
+          height: '100%',
+          borderRadius: 'inherit',
+          backgroundColor: 'white',
+          transform: 'scale(0)',
+          zIndex: '-1',
+        },
+        '.btn-bookNow:hover::before': {
+          transform: 'scale(3)',
+        },
+        '.btn-bookNow:active': {
+          transform: 'scale(1)',
+        },
+
+
+        '.btn-bookNowCenter': {
+          padding: '12px 24px',
+          fontSize: '1rem',
+          fontFamily: 'abeezee',
+          fontWeight: '0',
+          color: '#A5B4FC',
+          border: '2px solid #A5B4FC',
+          borderRadius: '9999px', // Full rounding
+          backgroundColor: 'white',
+          overflow: 'hidden',
+          cursor: 'pointer',
+        },
+        '.btn-bookNowCenter:hover': {
+          color: 'white',
+          transform: 'scale(1.1)',
+          boxShadow: '0 0px 20px rgba(193, 163, 98, 0.4)',
+        },
+        '.btn-bookNowCenter::before': {
+          content: '""',
+          position: 'absolute',
+          inset: '0',
+          width: '100%',
+          height: '100%',
+          borderRadius: 'inherit',
+          backgroundColor: '#A5B4FC',
+          transform: 'scale(0)',
+          zIndex: '-1',
+        },
+        '.btn-bookNowCenter:hover::before': {
+          transform: 'scale(3)',
+        },
+        '.btn-bookNowCenter:active': {
+          transform: 'scale(1)',
+        },
+      });
+    },
+  ],
+}
+
+
+
