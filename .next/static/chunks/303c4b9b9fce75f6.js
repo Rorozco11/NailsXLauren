ea6d644ccfd49e2f01362a669d947a3656687ae0
Rorozco11 <96450960+Rorozco11@users.noreply.github.com>
2025-10-17 00:39:33 -0400

    Add Vercel configuration and environment files

---
+++ .next/static/chunks/303c4b9b9fce75f6.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/b481b415d579a874.js",
+  "static/chunks/turbopack-c4e54f7cc26cae31.js"
+])
---
+++ .next/static/chunks/f67c30928dd41259.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/b481b415d579a874.js",
+  "static/chunks/turbopack-7020a77e236f4950.js"
+])
